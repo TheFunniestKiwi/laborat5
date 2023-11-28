85baba8ab9fd3adb91a16bb6b834160d23dc8ccb
student <student@student.test>
2023-11-28 13:47:41 +0100

    circle part

--- laborat5/CirclePart.swift
+++ laborat5/CirclePart.swift
@@ -9,12 +9,20 @@ import SwiftUI
 
 struct CirclePart: Shape {
     func path(in rect: CGRect) -> Path {
-        <#code#>
+        var path = Path()
+        
+        let width  = rect.midX
+        let heght = rect.midY
+        path.move(to: CGPoint(x: 200, y: 350))
+        path.addArc(center: CGPoint(x: 200, y: 350), radius: 150, startAngle: .degrees(160), endAngle: .degrees(90), clockwise: false)
+        
+        return path
     }
+}
     
 
-}
 
 #Preview {
     CirclePart()
+        .fill(.green)
 }
